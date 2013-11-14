cac25775f9572b404c73c0bd2b59a10a16b27f29
Yann Blanc <yann.blanc.38144@gmail.com>
2013-11-14 22:40:21 +0000

    Utilisation de la facade admin et test OK

--- src/ecomclientlourdadmin/EcomClientLourdAdmin.java
+++ src/ecomclientlourdadmin/EcomClientLourdAdmin.java
@@ -13,7 +13,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import session.interfaces.BeverageFacadeRemoteItf;
+import session.interfaces.AdminFacadeRemoteItf;
 
 /**
  *
@@ -29,15 +29,15 @@ public class EcomClientLourdAdmin {
             Properties props = new Properties();
             props.load(new FileInputStream("jndi.properties"));
             InitialContext ctx = new InitialContext(props);
-            BeverageFacadeRemoteItf beverageFacade = (BeverageFacadeRemoteItf) ctx.lookup("java:global/Ecom/Ecom-ejb/BeverageFacade!session.interfaces.BeverageFacadeRemoteItf");
+            AdminFacadeRemoteItf beverageFacade = (AdminFacadeRemoteItf) ctx.lookup("java:global/Ecom/Ecom-ejb/AdminFacadeBean!session.interfaces.AdminFacadeRemoteItf");
             System.out.println("message = " + beverageFacade.toString());
             BeverageEntity b = new BeverageEntity();
-            b.setAlcoholicDegree(42);
-            b.setName("Tequila");
-            b.setPrice(new Float(20));
-            b.setCapacity(5);
-            beverageFacade.create(b);
-            List<BeverageEntity> list = beverageFacade.getAllDrinks();
+            b.setAlcoholicDegree(99);
+            b.setName("Gnole de bach");
+            b.setPrice(new Float(1));
+            b.setCapacity(1000000);
+            beverageFacade.addBeverage(b);
+            List<BeverageEntity> list = beverageFacade.getAllBeverages();
             for (BeverageEntity dr : list) {
                 System.out.println(dr.getName() + " : \n Degré d'alcool : " + dr.getAlcoholicDegree() + " \n Prix : " + dr.getPrice() + "€");
             }
