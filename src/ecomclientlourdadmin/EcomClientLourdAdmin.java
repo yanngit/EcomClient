82c404bb767d8adda73c23928fb8e212afc6cf0e
Yann Blanc <yann.blanc.38144@gmail.com>
2013-11-15 15:19:55 +0000

    Attribut pour adminFacade et refactor du nom

--- src/ecomclientlourdadmin/EcomClientLourdAdmin.java
+++ src/ecomclientlourdadmin/EcomClientLourdAdmin.java
@@ -26,6 +26,8 @@ import session.interfaces.AdminFacadeRemoteItf;
  */
 public class EcomClientLourdAdmin {
 
+    static private AdminFacadeRemoteItf adminFacade;
+
     /**
      * @param args the command line arguments
      */
@@ -34,16 +36,14 @@ public class EcomClientLourdAdmin {
             Properties props = new Properties();
             props.load(new FileInputStream("jndi.properties"));
             InitialContext ctx = new InitialContext(props);
-            AdminFacadeRemoteItf beverageFacade = (AdminFacadeRemoteItf) ctx.lookup("java:global/Ecom/Ecom-ejb/AdminFacadeBean!session.interfaces.AdminFacadeRemoteItf");
-            EcomClientLourdAdmin.addCocktail(beverageFacade);
-        } catch (IOException ex) {
-            Logger.getLogger(EcomClientLourdAdmin.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (NamingException ex) {
+            adminFacade = (AdminFacadeRemoteItf) ctx.lookup("java:global/Ecom/Ecom-ejb/AdminFacadeBean!session.interfaces.AdminFacadeRemoteItf");
+            EcomClientLourdAdmin.addCocktail();
+        } catch (IOException | NamingException ex) {
             Logger.getLogger(EcomClientLourdAdmin.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    static public void addCocktail(AdminFacadeRemoteItf beverageFacade) {
+
+    static public void addCocktail() {
         CocktailEntity cocktail = new CocktailEntity();
         cocktail.setName("Cocktail Jamaïca");
         cocktail.setPrice(new Float(2.5));
@@ -51,35 +51,24 @@ public class EcomClientLourdAdmin {
         cocktail.setFlavor(CocktailFlavorEnum.FRUITY);
         cocktail.setPower(CocktailPowerEnum.SOFT);
         List<Deliverable> deliverables = new ArrayList<>();
-        deliverables.add((Deliverable) beverageFacade.getAllBeverages().get(0));
+        deliverables.add((Deliverable) adminFacade.getAllBeverages().get(0));
         cocktail.setDeliverables(deliverables);
         System.out.println("Adding cocktail : " + cocktail);
-        beverageFacade.addCocktail(cocktail);
+        adminFacade.addCocktail(cocktail);
         System.out.println("Done");
     }
-    
+
     static public void addBeverage() {
-        try {
-            Properties props = new Properties();
-            props.load(new FileInputStream("jndi.properties"));
-            InitialContext ctx = new InitialContext(props);
-            AdminFacadeRemoteItf beverageFacade = (AdminFacadeRemoteItf) ctx.lookup("java:global/Ecom/Ecom-ejb/AdminFacadeBean!session.interfaces.AdminFacadeRemoteItf");
-            System.out.println("message = " + beverageFacade.toString());
-            BeverageEntity b = new BeverageEntity();
-            b.setAlcoholicDegree(99);
-            b.setName("Gnole de bach");
-            b.setPrice(new Float(1));
-            b.setCapacity(1000000);
-            b.setQuantity(0);
-            beverageFacade.addBeverage(b);
-            List<BeverageEntity> list = beverageFacade.getAllBeverages();
-            for (BeverageEntity dr : list) {
-                System.out.println(dr.getName() + " : \n Degré d'alcool : " + dr.getAlcoholicDegree() + " \n Prix : " + dr.getPrice() + "€");
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(EcomClientLourdAdmin.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (NamingException ex) {
-            Logger.getLogger(EcomClientLourdAdmin.class.getName()).log(Level.SEVERE, null, ex);
+        BeverageEntity b = new BeverageEntity();
+        b.setAlcoholicDegree(99);
+        b.setName("Gnole de bach");
+        b.setPrice(new Float(1));
+        b.setCapacity(1000000);
+        b.setQuantity(0);
+        adminFacade.addBeverage(b);
+        List<BeverageEntity> list = adminFacade.getAllBeverages();
+        for (BeverageEntity dr : list) {
+            System.out.println(dr.getName() + " : \n Degré d'alcool : " + dr.getAlcoholicDegree() + " \n Prix : " + dr.getPrice() + "€");
         }
     }
 }
